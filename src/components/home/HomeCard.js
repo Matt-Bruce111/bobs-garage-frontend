130405b3632de1be2661cb1670730beb4fcf0b7b
Matt-Bruce111 <Matt.bruce111@gmail.com>
2023-07-10 13:20:48 +1000

    Fixes

--- src/components/home/HomeCard.js
+++ src/components/home/HomeCard.js
@@ -5,7 +5,7 @@ const HomeCard = ({ title, body, link }) => {
   return (
     <div className='card text-center d-flex justify-content-center h-100' id='homecard'>
       <div className='card-body w-75 mx-auto' >
-        <h3 className='card-title mx-auto w-75'>{title}</h3>
+        <h3 className='card-title mx-auto'>{title}</h3>
         <p className='my-4 fs-5'>{body}</p>
         <Link to={link.link} className="btn d-flex justify-content-center my-4" ><p className='fs-5 my-0'>{link.text}</p></Link>
       </div>
